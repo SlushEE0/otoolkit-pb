56c134f8319a053266b57482fab5fd6e4e974174
SlushEE0 <neeladem2009@gmail.com>
2025-08-08 15:47:30 -0700

    allow for discord oauth

--- pb_migrations/1752469945_oauth_setup.js
+++ pb_migrations/1752469945_oauth_setup.js
@@ -1,21 +1,52 @@
 /// <reference path="../../otoolkit-pb/pb_data/types.d.ts" />
 migrate(
   (app) => {
-    const ClientID = $os.getenv("GOOGLE_CLIENT_ID") || "";
-    const ClientSecret = $os.getenv("GOOGLE_CLIENT_SECRET") || "";
+    const ClientID_google = $os.getenv("GOOGLE_CLIENT_ID") || "";
+    const ClientSecret_google = $os.getenv("GOOGLE_CLIENT_SECRET") || "";
 
-    const providers = [
-      {
+    const ClientID_discord = $os.getenv("DISCORD_CLIENT_ID") || "";
+    const ClientSecret_discord = $os.getenv("DISCORD_CLIENT_SECRET") || "";
+
+    let providers = [];
+
+    if (ClientID_google && ClientSecret_google) {
+      providers.push({
         pkce: null,
         name: "google", // or whatever provider you want
-        clientId: ClientID,
-        clientSecret: ClientSecret,
+        clientId: ClientID_google,
+        clientSecret: ClientSecret_google,
         authUrl: "",
         tokenUrl: "",
         userApiUrl: "",
         displayName: ""
-      }
-    ];
+      });
+    } else {
+      console.warn(
+        "[PB Init]: Google OAuth2 credentials are not set. Skipping Google provider setup."
+      );
+    }
+
+    if (ClientID_discord && ClientSecret_discord) {
+      providers.push({
+        pkce: null,
+        name: "discord", // or whatever provider you want
+        clientId: ClientID_discord,
+        clientSecret: ClientSecret_discord,
+        authUrl: "https://discord.com/api/oauth2/authorize",
+        tokenUrl: "https://discord.com/api/oauth2/token",
+        userApiUrl: "https://discord.com/api/users/@me",
+        displayName: "Discord"
+      });
+    } else {
+      console.warn(
+        "[PB Init]: Discord OAuth2 credentials are not set. Skipping Discord provider setup."
+      );
+    }
+
+    if (providers.length === 0) {
+      console.warn("[PB Init] No OAuth2 providers are configured.");
+      return;
+    }
 
     const users = app.findCollectionByNameOrId("users");
     users.oauth2.providers = providers;
